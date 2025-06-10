27037b4f14baa60045421b069e600162011cf815
shafee_azeem <shafeeghani@gmail.com>
2025-06-10 16:48:48 +0530

    More practice

--- dsa.js
+++ dsa.js
@@ -242,3 +242,88 @@ const findTheLongestSS = (str) => {
 }
 
 console.log(findTheLongestSS(string8))
+
+
+console.log("*********** problem 9")
+const numbers9 = [12, 45, 23, 56, 77, 23, 12, 1, 3, 89];
+
+const findThebiggestNumber = (arr) => {
+    let biggest = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > biggest) {
+            biggest = arr[i];
+        }
+    }
+    return biggest;
+}
+
+console.log(findThebiggestNumber(numbers9));
+
+console.log("******* Bubble sort ***** Problem 10")
+const numbers10 = [7, 8, 9, 4, 5, 2, 7, 6, 1];
+
+function bubbleSort(arr) {
+    let wasSwapped;
+    let pass = 0;
+
+    do {
+
+        wasSwapped = false;
+        pass++;
+
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                //swapping 2 Elements
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+
+                wasSwapped = true;
+
+            }
+
+        }
+
+        arr.forEach(num => {
+            console.log("|".repeat(num).padEnd(10, '') + `${num}`)
+        });
+
+    } while (wasSwapped)
+
+    return arr;
+
+}
+
+const sortedArr = bubbleSort(numbers10);
+console.log(`\n Final Sorted array:`, sortedArr);
+
+
+console.log("Bubble Sort visualization******** ");
+
+const number10b = [4, 5, 6, 3, 2, 7, 9, 1, 8];
+
+function bubbleSort10b(arr) {
+    let wasSwapped;
+
+    do {
+        let wasSwapped = false;
+
+        for (let i = 0; i < arr.length - 1; i++) {
+
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                wasSwapped = true;
+
+            }
+
+        }
+
+    } while (wasSwapped)
+    return arr;
+
+}
+
+console.log(bubbleSort10b(number10b))
