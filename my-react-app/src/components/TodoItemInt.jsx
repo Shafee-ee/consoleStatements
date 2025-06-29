8b7ece6052aa78be55839ca49890814d70d12c8d
shafee_azeem <shafeeghani@gmail.com>
2025-06-29 23:49:18 +0530

    more practice

--- my-react-app/src/components/TodoItemInt.jsx
+++ my-react-app/src/components/TodoItemInt.jsx
@@ -6,14 +6,14 @@ const TodoItemInt = React.memo(({ todo, onToggle, onDelete }) => {
     return (
         <div className="flex justify-between items-center px-4 py-2 border-b">
             <span
-                className={`flex-1 cursor-pointer ${todo.done ? 'line-through text-gray-500' : ''}`}
+                className={`flex-1 text-blue-800 cursor-pointer ${todo.done ? 'line-through text-red-300' : ''}`}
                 onClick={() => onToggle(todo.id)}
             >
                 {todo.text}
             </span>
             <button
                 onClick={() => onDelete(todo.id)}
-                className="text-red-500 hover:underline ml-4"
+                className="text-red-500 ml-4 bg-red-200 px-2 hover:text-white hover:bg-red-400 hover:rounded"
             >
                 Delete
             </button>
--- my-react-app/src/components/UpdateTitle.jsx
+++ my-react-app/src/components/UpdateTitle.jsx
@@ -4,12 +4,12 @@ const UpdateTitle = () => {
     const [clicks, setClicks] = useState(0);
 
     useEffect(() => {
-        document.title = `Click No:${clicks}`;
+        document.title = `${clicks}`;
     }, [clicks]);
 
     return (
         <div>
-            <button onClick={() => { setClicks(prev => prev + 1) }}>Click</button>
+            <button className="bg-green-500 px-6  py-2 hover:bg-green-400 hover:text-white" onClick={() => { setClicks(prev => prev + 1) }}>Click</button>
         </div>
     )
 }
